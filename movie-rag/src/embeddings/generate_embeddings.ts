adccc84096b65bed41464388f9574ffeaea166ed
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-11-26 11:09:49 +0000

    Ingesting basic documents

--- movie-rag/src/embeddings/generate_embeddings.ts
+++ movie-rag/src/embeddings/generate_embeddings.ts
@@ -1,100 +1,124 @@
 import {
-    ElasticVectorSearch,
-    type ElasticClientArgs,
-  } from "@langchain/community/vectorstores/elasticsearch";
-  import { Document } from "@langchain/core/documents";
-  
-  import { OllamaEmbeddings } from "@langchain/ollama";
-  
-  import { Client, type ClientOptions } from "@elastic/elasticsearch";
-  
-  import * as fs from "node:fs";
-  
-  // Initialize Ollama embeddings
-  const ollamaEmbeddings = new OllamaEmbeddings({
-    model: "mxbai-embed-large", // Default value
-    //baseUrl: "http://localhost:11434", // Default value
-  });
-  
-  // Initialize Langchain and Elasticsearch
-  const config: ClientOptions = {
-    node: process.env.ELASTIC_DEPLOYMENT,
+  ElasticVectorSearch,
+  type ElasticClientArgs,
+} from "@langchain/community/vectorstores/elasticsearch";
+import { Document } from "@langchain/core/documents";
+
+import { OllamaEmbeddings } from "@langchain/ollama";
+
+import { Client, type ClientOptions } from "@elastic/elasticsearch";
+
+import fs from "node:fs";
+import { MovieCollection } from "./movies";
+
+// Initialize Ollama embeddings
+const ollamaEmbeddings = new OllamaEmbeddings({
+  model: "mxbai-embed-large", // Default value
+  //baseUrl: "http://localhost:11434", // Default value
+});
+
+// Initialize Langchain and Elasticsearch
+const config: ClientOptions = {
+  node: process.env.ELASTIC_DEPLOYMENT,
+};
+
+if (process.env.ELASTIC_API_KEY) {
+  config.auth = {
+    apiKey: process.env.ELASTIC_API_KEY,
   };
-  
-  if (process.env.ELASTIC_API_KEY) {
-    config.auth = {
-      apiKey: process.env.ELASTIC_API_KEY,
-    };
-  } 
-  
-  const clientArgs: ElasticClientArgs = {
-    client: new Client(config),
-    indexName: "test", //"movies",
-    vectorSearchOptions: {
-        engine: "hnsw",
-        similarity: "dot_product", //Default cosine
-    }
-  };
-  
-  const vectorStore = new ElasticVectorSearch(ollamaEmbeddings, clientArgs);
-
-// Load documents and split
-const document1 = new Document({
-    pageContent: "This is a document",
-    metadata: { source: "tweet" },
-});
-
-const document2 = new Document({
-    pageContent: "This is another document",
-    metadata: { source: "news" },
-});
-
-const document3 = new Document({
-    pageContent: "This is a big news story!",
-    metadata: { source: "news" },
-});
-
-const documents: Document[] = [ document1, document2, document3 ];
-
-// Ingest document with embedding
-async function generateEmbeddings(): Promise<string[]> {
-    try {
-        return await vectorStore.addDocuments(documents);
-    } catch(e) {
-        console.log(e);
-        return [];
-    }
 }
 
-async function findRelevantNews(text: string): Promise<Document[]> {
-    try {
-        const filter = [
-            {
-              operator: "match",
-              field: "source",
-              value: "news",
-            },
-          ];
-          
-          const similaritySearchResults = await vectorStore.similaritySearch(
-            text,
-            1,
-            filter
-          );
+const clientArgs: ElasticClientArgs = {
+  client: new Client(config),
+  indexName: process.env.INDEX_NAME,
+  vectorSearchOptions: {
+    engine: "hnsw",
+    similarity: "dot_product", //Default cosine
+  },
+};
 
-          return similaritySearchResults
-    } catch(e) {
-        console.log(e);
-        return [];
-    }
+const vectorStore = new ElasticVectorSearch(ollamaEmbeddings, clientArgs);
+
+function generateDocumentsFromJson(pathToJSON: string): Document[] {
+  try {
+    const jsonDocs: MovieCollection = JSON.parse(
+      fs.readFileSync(pathToJSON).toString()
+    );
+    console.log(`Doc count: ${jsonDocs.results.length}`);
+
+    // TODO add splitter?
+
+    return jsonDocs.results.map((doc) => {
+      return new Document({
+        pageContent: doc.overview,
+        metadata: {
+          title: doc.title,
+          original_language: doc.original_language,
+          popularity: doc.popularity,
+          releaseDate: new Date(doc.release_date),
+          vote_average: doc.vote_average,
+          vote_count: doc.vote_count,
+          isAdult: doc.adult,
+          posterPath: `https://image.tmdb.org/t/p/original${doc.poster_path}`
+        },
+      });
+    });
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+}
+
+// Ingest document with embedding
+async function generateEmbeddings(documents: Document[]): Promise<string[]> {
+  try {
+    return await vectorStore.addDocuments(documents);
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+}
+
+async function findRelevantMovies(text: string): Promise<Document[]> {
+  try {
+    const filter = [
+      {
+        operator: "match",
+        field: "isAdult",
+        value: false,
+      },
+    ];
+
+    const similaritySearchResults = await vectorStore.similaritySearch(
+      text,
+      1,
+      filter
+    );
+
+    return similaritySearchResults;
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
 }
 
 async function main() {
-    const ids = await generateEmbeddings();
-    console.log(`Ingested ${ids.length} documents with embeddings`);
+  // Clean up index if it exists
+  vectorStore.deleteIfExists();
 
-    const bigNewsStory = await findRelevantNews("Find me the biggest news story!");
-    console.log(`The biggest news story is: "${bigNewsStory[0].pageContent}"`)
+  // Load data from JSON
+  const documents = generateDocumentsFromJson("./data/movies.json");
+
+  // Ingest documents and generate embeddings
+  const ids = await generateEmbeddings(documents);
+  console.log(`Ingested ${ids.length} documents with embeddings`);
+
+  // Example retrieval
+  const topMovieResult = await findRelevantMovies(
+    "Find me a movie featuring a dark anti-hero"
+  );
+
+  console.log(`The anti-hero movie is: "${topMovieResult[0].metadata.title}"`);
 }
 
 main();
---
+++ movie-rag/src/embeddings/movies.ts
@@ -0,0 +1,23 @@
+export interface MovieCollection {
+    page: number
+    results: Movie[]
+    total_pages: number
+    total_results: number
+  }
+  
+  export interface Movie {
+    adult: boolean
+    backdrop_path: string
+    genres: number[]
+    id: number
+    original_language: string
+    original_title: string
+    overview: string
+    popularity: number
+    poster_path: string
+    release_date: string
+    title: string
+    video: boolean
+    vote_average: number
+    vote_count: number
+  }
