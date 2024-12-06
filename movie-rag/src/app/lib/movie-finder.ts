fe470753911c4067209d0ecb53c13fa59b814124
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-12-06 14:54:14 +0000

    Stripping solution code out

--- movie-rag/src/app/lib/movie-finder.ts
+++ movie-rag/src/app/lib/movie-finder.ts
@@ -12,53 +12,6 @@ import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 
 // Initialize Ollama embeddings for the query
-const ollamaEmbeddings = new OllamaEmbeddings({
-  model: "mxbai-embed-large", // Default value
-  //baseUrl: "http://localhost:11434", // Default value
-});
-
-// Initialize Langchain and Elasticsearch
-const config: ClientOptions = {
-  node: process.env.ELASTIC_DEPLOYMENT,
-};
-
-if (process.env.ELASTIC_API_KEY) {
-  config.auth = {
-    apiKey: process.env.ELASTIC_API_KEY,
-  };
-}
-
-const clientArgs: ElasticClientArgs = {
-  client: new Client(config),
-  indexName: process.env.INDEX_NAME,
-  vectorSearchOptions: {
-    engine: "hnsw",
-    similarity: "cosine", //Default cosine
-  },
-};
-
-const vectorStore = new ElasticVectorSearch(ollamaEmbeddings, clientArgs);
-
-// Initialise LLM and prompt
-const template = `You are a helpful movie trivial assistant that loves to recommend movies to people. 
-      Check your knowledge base before answering any questions.
-      If no relevant information is found in the tool calls, respond, "I don't know, sorry!"
-      
-      Please use the below context in your answer:
-      <context>
-      {context}
-      </context>`;
-
-const prompt = ChatPromptTemplate.fromTemplate(template);
-
-const llm = new Ollama({
-  model: "llama3", // Default: "llama3",
-  temperature: 0,
-  maxRetries: 3,
-});
-
-// Chat History
-const chatHistory = new ChatMessageHistory();
 
 /**
  * Example search function to find relevant movies
